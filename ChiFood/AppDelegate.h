2df1b760808ff5769a6dd35bd7894e727be57b76
Bradley Golden <golden.bradley@gmail.com>
2016-01-25 18:50:41 -0600

    Initial Commit

---
+++ ChiFood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChiFood
+//
+//  Created by Bradley Golden on 1/25/16.
+//  Copyright © 2016 Bradley Golden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChiFood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChiFood
+//
+//  Created by Bradley Golden on 1/25/16.
+//  Copyright © 2016 Bradley Golden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
